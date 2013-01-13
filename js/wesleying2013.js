0cdf6f8374446418704491879c549b2556313734
Will Durney <futurityverb@gmail.com>
2013-01-13 14:32:09 -0500

    Added scrolling script for featured posts bar

--- js/wesleying2013.js
+++ js/wesleying2013.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function() {
     // initialization code goes here
-    console.log("This was loaded");
-    jQuery('div#featured-bar a').map(function() { return jQuery(this).data().postid });
+    console.log("This was loaded!");
+    //jQuery('div#featured-bar a').map(function() { return jQuery(this).data().postid });
+    
+    // hijack featured post links
+    jQuery('div#featured-bar a').click(function(event) {
+      var $link = jQuery(this);
+      var postid = $link.data().postid;
+      var $article = jQuery('article#post-'+postid);
+      // see if the article exists on the page
+      if($article.length != 0) {
+        event.preventDefault();
+        jQuery('html, body').animate({
+          scrollTop: $article.offset().top
+        }, 1000);
+      }
+    });
 });
