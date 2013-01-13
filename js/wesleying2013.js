22f21ff5416d7270ac3a3ac7a1a278554be858d5
Evan Carmi <evan@ecarmi.org>
2013-01-13 12:53:51 -0800

    Shortened scroll duration

--- js/wesleying2013.js
+++ js/wesleying2013.js
@@ -1,9 +1,6 @@
 jQuery(document).ready(function() {
-    // initialization code goes here
-    console.log("This was loaded!");
-    //jQuery('div#featured-bar a').map(function() { return jQuery(this).data().postid });
-    
     // hijack featured post links
+    var scrollDuration = 500;
     jQuery('div#featured-bar a').click(function(event) {
       var $link = jQuery(this);
       var postid = $link.data().postid;
@@ -13,7 +10,7 @@ jQuery(document).ready(function() {
         event.preventDefault();
         jQuery('html, body').animate({
           scrollTop: $article.offset().top
-        }, 1000);
+        }, scrollDuration);
       }
     });
 });
