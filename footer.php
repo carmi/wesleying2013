17bbb047de10e3fa1e0d20b4e2637fdf0d357174
Evan Carmi <evan@ecarmi.org>
2013-01-17 23:24:21 -0800

    grammar

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
   <div class="site-info">
     <span><a href="<?php get_site_url(); ?>">Wesleying</a> is designed, powered<span title="Who gives a fuck about an oxford comma? Wesleying does.">,</span> and written by current Wesleyan students.<span>
         <?php wp_loginout(); ?>.
-        <a href="#" class="back-to-top" title="Yes, a button that instantly takes you to the top is cool, however this does not mean we endorse the socially contructed 'top is better' hegemonic neoliberalist idealogy.">
+        <a href="#" class="back-to-top" title="Yes, a button that instantly takes you to the top is cool; however, this does not mean we endorse the socially contructed 'top is better' hegemonic neoliberalist idealogy.">
           <button class="back-to-top">Top <i class="icon-double-angle-up"></i></button></a>
       </div><!-- .site-info -->
       </footer><!-- #colophon -->
